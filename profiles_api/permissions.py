d5428fea1fed3d27e7f8430a075ac5b2d7a4b2e8
Sydney <s.swofford@eugenustech.com>
2025-11-10 19:30:19 -0800

    addded user profile feed api

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -9,3 +9,12 @@ class UpdateOwnProfile(permissions.BasePermission):
         
         return obj.id==request.user.id
 
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow Users to update their own status"""
+
+    def has_object_permission(sef, request, view ,obj):
+        """Check user is trying to update own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.user_profile.id==request.user.id
